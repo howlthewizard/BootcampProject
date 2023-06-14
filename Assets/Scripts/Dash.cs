4c08588ef7dac3b167e394f11e6d68e03905f5c0
howlthewizard <alperrakn24@gmail.com>
2023-06-14 11:15:18 +0300

    Alper Movement

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public float dashForce = 10f; // Dash hýzý
+    private bool isDashing = false;
+    private Rigidbody rb;
+    private PlayerMovingScript playerMovement;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerMovement = GetComponent<PlayerMovingScript>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isDashing = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            Dashing();
+            rb.velocity = Vector3.zero;
+        }
+        /*else if(playerMovement.isMoving)
+        {
+           rb.velocity = Vector3.zero;
+        }*/
+    }
+
+    private void Dashing()
+    {
+        rb.AddForce(transform.forward * dashForce,ForceMode.Impulse);
+        isDashing = false;
+    }
+
+}
