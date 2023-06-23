b07940f6cbed6d84b6a9abab051cc04266601f85
Aybala Kılınç <ay.balakilinc91@gmail.com>
2023-06-23 23:38:50 +0300

    main menu1

---
+++ Assets/Aybala/Scripts/MenuPanel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Aybala.Scripts
+{
+    public class MenuPanel : MonoBehaviour
+    {
+        public GameObject optionsScreen;
+    
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Oyundan Ciktik!");
+            Application.Quit();
+        }
+
+        public void ReturnToMainMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void OpenOptions()
+        {
+            optionsScreen.SetActive(true);  
+        }
+
+        public void CloseOptions()
+        {
+            optionsScreen.SetActive(false);
+        }
+    }
+}
