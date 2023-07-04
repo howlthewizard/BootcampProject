b605a792ad073e7bf07b2abca164e7c1202b6325
howlthewizard <alperrakn24@gmail.com>
2023-07-04 23:56:00 +0300

    newjump

--- Assets/Scripts/Movement/Jump.cs
+++ Assets/Scripts/Movement/Jump.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Jump : MonoBehaviour
 {
     public Rigidbody rb;
+    public NavMeshAgent navMeshAgent;
     public bool isGrounded = true;
+    public bool isJumping = false;
     public float fallMultiplier = 5000f;
     public float lowJumpMultiplier = 4000f;
     public float jumpForce = 50;
@@ -13,17 +16,7 @@ public class Jump : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        if(rb.velocity.y < 0)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.deltaTime;
-        }else if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * lowJumpMultiplier * Time.deltaTime;
-        }
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void FixedUpdate()
@@ -32,14 +25,30 @@ public class Jump : MonoBehaviour
         {
             rb.velocity = Vector3.up * jumpForce;
             isGrounded = false;
+            isJumping = true;
+            navMeshAgent.enabled = false;
         }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * lowJumpMultiplier * Time.deltaTime;
+        }
+
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if(collision.gameObject.tag == "Ground" && isJumping)
         {
             isGrounded = true;
+            isJumping = false;
+            navMeshAgent.enabled = true;
         }
     }
+
 }
