ae374357359bf70af9fb4833d6952c9a23a6abeb
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-08 16:55:19 +0300

    Revert "OldWiseMan Dialogue system added"
    This reverts commit 25d39662d9ff6c6a3537fea364ff83a3cdca52ed.

--- Assets/Aybala/Scripts/DialogueTrigger.cs
+++ Assets/Aybala/Scripts/DialogueTrigger.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-      public Message[] messages;
+      public Message[] mesagges;
       public Actor[] actors;
 
       public void StartDialogue()
       {
-        DialogueManager.instance.OpenDialogue(messages, actors);
-    }
+            FindObjectOfType<DialogueManager>().OpenDialogue(mesagges, actors);
+      }
      
 }
 
