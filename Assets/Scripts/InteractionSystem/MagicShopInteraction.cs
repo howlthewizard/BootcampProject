da023ad78d5de4a0e302439b0a2109b54079dbe8
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-13 12:42:06 +0300

    Magic Shopkeeper changes

---
+++ Assets/Scripts/InteractionSystem/MagicShopInteraction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicShopInteraction : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+    [SerializeField] private GameObject shopPanel;
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        shopPanel.SetActive(true);
+        return true;
+    }
+}
