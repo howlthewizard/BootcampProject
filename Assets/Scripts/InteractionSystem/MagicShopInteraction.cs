02c1dfa89779520e4b19b7cde95ec4d7ce3713f8
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-13 13:22:38 +0300

    ShopInteraction bugfix

--- Assets/Scripts/InteractionSystem/MagicShopInteraction.cs
+++ Assets/Scripts/InteractionSystem/MagicShopInteraction.cs
@@ -1,3 +1,5 @@
+using AI.Controller;
+using AI.Shops;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +8,18 @@ public class MagicShopInteraction : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
     [SerializeField] private GameObject shopPanel;
+    [SerializeField] private PlayerCursorController callingController;
+    private Shop shop;
     public string InteractionPrompt => _prompt;
 
+    private void Awake()
+    {
+        shop = GetComponent<Shop>();
+    }
     public bool Interact(Interactor interactor)
     {
         shopPanel.SetActive(true);
+        callingController.GetComponent<Shopper>().SetActiveShop(shop);
         return true;
     }
 }
