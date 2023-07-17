f36d636750d5e97dbd5e836835a04d5c5425628b
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-17 16:51:00 +0300

    End Game panel bugfix

---
+++ Assets/Scripts/EndGameInteraction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameInteraction : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+    [SerializeField] private Canvas endGamePanel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            trigger.StartDialogue();
+           
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            endGamePanel.gameObject.SetActive(true);
+        }
+    }
+}
